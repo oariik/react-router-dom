7914f0ccf7982802f9fa6b3f7f561b6d1716e974
Oguzhan Arik <33468658+oariik@users.noreply.github.com>
2018-08-09 14:17:23 +0300

    react-router-dom
    simple project

---
+++ react-router/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter as Router, Route, Link, NavLink, Redirect } from 'react-router-dom';
+import News from './components/News';
+import Profile from './components/Profile';
+import Error from './components/Error';
+
+
+
+
+class App extends Component {
+  state = {
+    loggedIn: false
+  };
+
+  onClickButton = () => {
+    this.setState(prevState => ({
+      loggedIn: !prevState.loggedIn
+    }))
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavLink activeClassName="activelink" exact to="/">Homepage</NavLink> <br />
+          <NavLink activeClassName="activelink" exact to="/contact">Contact</NavLink> <br />
+          <NavLink activeClassName="activelink" exact to="/news/2">News</NavLink> <br />
+          <NavLink activeClassName="activelink" exact to="/profile">Profile</NavLink>
+
+          <br /><br />
+
+          <input
+            type="button"
+            onClick={this.onClickButton}
+            value={this.state.loggedIn ? 'Logout' : 'Login'}
+          />
+
+          <Route path="/" exact render={
+            () => {
+              return (<h1>Home page</h1>)
+            }
+          } />
+
+          <Route path="/contact" exact strict render={
+            () => {
+              return (<h1>Contact page</h1>)
+            }
+          } />
+
+          <Route path="/news/:id" exact strict component={News} />
+
+          <Route path="/profile" exact strict render={() => (
+            this.state.loggedIn ? (<Profile />) : (<Redirect to="/" />)
+          )} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ react-router/src/components/News.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+
+const News = ({ match }) =>
+    <h1>News page: {match.params.id}</h1>;
+
+export default News;
